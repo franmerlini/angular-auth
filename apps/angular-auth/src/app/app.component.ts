39b9020c1701f500a1a220b8fbfe0408175a8e35
Francisco Merlini <merlinifrancisco@gmail.com>
2023-11-12 03:30:27 -0300

    refactor: remove unused code and update dependencies

--- apps/angular-auth/src/app/app.component.ts
+++ apps/angular-auth/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { WebShellModule } from '@angular-auth/web/shell/feature';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [BrowserModule, WebShellModule],
   selector: 'angular-auth-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `<router-outlet />`,
 })
-export class AppComponent {
-  title = 'angular-auth';
-}
+export class AppComponent {}
