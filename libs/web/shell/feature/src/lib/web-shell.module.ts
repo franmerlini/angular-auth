97910b1a626930e9949c471776709801da0fda12
Francisco Merlini <merlinifrancisco@gmail.com>
2023-11-12 02:54:12 -0300

    feat: add web shell lib

---
+++ libs/web/shell/feature/src/lib/web-shell.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { webShellFeatureRoutes } from './web-shell.routes';
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(webShellFeatureRoutes)],
+})
+export class WebShellFeatureModule {}
