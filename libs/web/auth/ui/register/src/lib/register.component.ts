571ac8f8ddf82696d1e80d77730603dac6a61be5
Francisco Merlini <merlinifrancisco@gmail.com>
2023-11-27 18:22:51 -0300

    feat: add register ui lib

---
+++ libs/web/auth/ui/register/src/lib/register.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'aa-register',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>register works!</p>`,
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterComponent {}
