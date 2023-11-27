5706bedb9c2d09b150c108df325ffebc02222743
Francisco Merlini <merlinifrancisco@gmail.com>
2023-11-26 23:26:49 -0300

    refactor: delete unused module

--- apps/angular-auth/src/app/app.component.ts
+++ apps/angular-auth/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-
-import { WebShellModule } from '@angular-auth/web/shell/feature';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [WebShellModule, RouterModule],
+  imports: [RouterModule],
   selector: 'angular-auth-root',
   template: `<router-outlet />`,
 })
