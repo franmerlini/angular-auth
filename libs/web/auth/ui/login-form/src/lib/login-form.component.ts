11516134386db94d8c48ac3e89c63d72463a7a7b
Francisco Merlini <merlinifrancisco@gmail.com>
2023-12-22 19:47:10 -0300

    feat(auth): add login form template

--- libs/web/auth/ui/login-form/src/lib/login-form.component.ts
+++ libs/web/auth/ui/login-form/src/lib/login-form.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'aa-login-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterLink],
   templateUrl: './login-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
