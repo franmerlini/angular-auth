5efc6711b6420c2f96192d57e95c03e322efb3c4
Francisco Merlini <merlinifrancisco@gmail.com>
2023-12-22 21:55:35 -0300

    feat(shared): add store lib

--- apps/angular-auth/src/app/app.config.ts
+++ apps/angular-auth/src/app/app.config.ts
@@ -1,8 +1,28 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+import {
+  CustomSerializer,
+  ROOT_EFFECTS,
+  ROOT_REDUCERS,
+  RouterFeatureKey,
+} from '@angular-auth/libs/web/shared/store';
 import { webShellRoutes } from '@angular-auth/web/shell/feature';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(webShellRoutes)],
+  providers: [
+    provideStore(ROOT_REDUCERS),
+    provideEffects(ROOT_EFFECTS),
+    provideRouterStore({
+      stateKey: RouterFeatureKey,
+      serializer: CustomSerializer,
+    }),
+    provideStoreDevtools(),
+    provideRouter(webShellRoutes),
+  ],
 };
---
+++ libs/web/shared/store/README.md
@@ -0,0 +1,7 @@
+# web-shared-store
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test web-shared-store` to execute the unit tests.
