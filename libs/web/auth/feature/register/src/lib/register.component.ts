be318df4b1b6dd2c72f6f5cef0e60c2f8cabd773
Francisco Merlini <merlinifrancisco@gmail.com>
2023-12-24 18:38:58 -0300

    refactor(auth): add register form controls

--- libs/web/auth/feature/register/src/lib/register.component.ts
+++ libs/web/auth/feature/register/src/lib/register.component.ts
@@ -1,17 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { Store } from '@ngrx/store';
+
+import { CreateUserDTO } from '@angular-auth/libs/common';
+import { AuthStore } from '@angular-auth/libs/web/auth/data-access/store';
 import { RegisterFormComponent } from '@angular-auth/libs/web/auth/ui/register-form';
+import { countriesFeature } from '@angular-auth/libs/web/shared/data-access/store';
 
 @Component({
   selector: 'aa-register',
   standalone: true,
-  imports: [RegisterFormComponent, RouterLink],
+  imports: [RegisterFormComponent, RouterLink, AsyncPipe],
   template: `
     <div class="min-h-screen flex flex-col justify-center items-center">
-      <aa-register-form />
+      @if (countries$ | async; as countries) {
+      <aa-register-form
+        [countryList]="countries"
+        (registerUser)="onRegisterUser($event)"
+      />
+      }
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  private readonly authStore = inject(AuthStore);
+  private readonly store = inject(Store);
+
+  countries$ = this.store.select(countriesFeature.selectAll);
+
+  onRegisterUser(user: CreateUserDTO): void {
+    this.authStore.register(user);
+  }
+}
--- libs/web/auth/ui/register-form/src/lib/register-form.component.ts
+++ libs/web/auth/ui/register-form/src/lib/register-form.component.ts
@@ -1,11 +1,88 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  inject,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
+import { Country, CreateUserDTO } from '@angular-auth/libs/common';
+
+type RegisterForm = {
+  firstName: FormControl<string>;
+  lastName: FormControl<string>;
+  email: FormControl<string>;
+  password: FormControl<string>;
+  confirmPassword: FormControl<string>;
+  city: FormControl<string>;
+  country: FormControl<number>;
+  acceptTerms: FormControl<boolean>;
+};
 
 @Component({
   selector: 'aa-register-form',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterFormComponent {}
+export class RegisterFormComponent implements OnInit {
+  @Input({ required: true }) countryList!: Country[];
+
+  @Output() registerUser = new EventEmitter<CreateUserDTO>();
+
+  private readonly fb = inject(NonNullableFormBuilder);
+
+  form!: FormGroup<RegisterForm>;
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.form = this.fb.group<RegisterForm>({
+      firstName: this.fb.control('', [Validators.required]),
+      lastName: this.fb.control('', [Validators.required]),
+      email: this.fb.control('', [Validators.email, Validators.required]),
+      password: this.fb.control('', [Validators.required]),
+      confirmPassword: this.fb.control('', [Validators.required]),
+      city: this.fb.control('', [Validators.required]),
+      country: this.fb.control(0, [Validators.required]),
+      acceptTerms: this.fb.control(false, [Validators.requiredTrue]),
+    });
+  }
+
+  register(event: SubmitEvent): void {
+    event.preventDefault();
+
+    if (this.form.invalid || this.acceptTerms.invalid) {
+      return;
+    }
+
+    const { firstName, lastName, email, password, city, country } =
+      this.form.getRawValue();
+
+    this.registerUser.emit({
+      firstName,
+      lastName,
+      email,
+      password,
+      city,
+      country: this.countryList.find(({ id }) => id === country) as Country,
+    });
+  }
+
+  get acceptTerms(): FormControl<boolean> {
+    return this.form?.get('acceptTerms') as FormControl<boolean>;
+  }
+}
