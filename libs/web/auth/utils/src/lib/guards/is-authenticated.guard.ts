b867142adbc8efd9fc13280e9377c875f77d8434
Francisco Merlini <merlinifrancisco@gmail.com>
2023-12-24 18:35:37 -0300

    refactor(auth): update import path

--- libs/web/auth/utils/src/lib/guards/is-authenticated.guard.ts
+++ libs/web/auth/utils/src/lib/guards/is-authenticated.guard.ts
@@ -6,7 +6,7 @@ import { Store } from '@ngrx/store';
 import { tap } from 'rxjs';
 
 import { AuthStore } from '@angular-auth/libs/web/auth/data-access/store';
-import { RouterActions } from '@angular-auth/libs/web/shared/store';
+import { RouterActions } from '@angular-auth/libs/web/shared/data-access/store';
 
 export const isAuthenticated = (): CanActivateFn => () => {
   const authStore = inject(AuthStore);
