51e6de85303d3017e32d1bf122b92664cb597990
Francisco Merlini <merlinifrancisco@gmail.com>
2023-12-25 13:58:18 -0300

    refactor(web-shared): remove mocked data

--- libs/web/shared/data-access/api/src/lib/auth.service.ts
+++ libs/web/shared/data-access/api/src/lib/auth.service.ts
@@ -1,25 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { CreateUserDTO, User } from '@angular-auth/libs/common';
 import { APP_CONFIG } from '@angular-auth/libs/web/shared/utils';
-
-const mockedUser: User = {
-  id: 1,
-  firstName: 'John',
-  lastName: 'Doe',
-  email: 'johndoe@mail.com',
-  city: 'New York',
-  country: {
-    id: 1,
-    name: 'United States',
-    code: 'US',
-  },
-  role: 'admin',
-  token: '1234567890',
-};
 
 @Injectable({
   providedIn: 'root',
@@ -30,14 +15,10 @@ export class AuthService {
   private readonly baseURL = `${this.appConfig.baseURL}/auth`;
 
   register(user: CreateUserDTO): Observable<User> {
-    console.log(user);
-    // return this.http.post<User>(`${this.baseURL}/register`, { user });
-    return of(mockedUser);
+    return this.http.post<User>(`${this.baseURL}/register`, { user });
   }
 
   login(email: string, password: string): Observable<User> {
-    console.log(email, password);
-    // return this.http.post<User>(`${this.baseURL}/login`, { email, password });
-    return of(mockedUser);
+    return this.http.post<User>(`${this.baseURL}/login`, { email, password });
   }
 }
