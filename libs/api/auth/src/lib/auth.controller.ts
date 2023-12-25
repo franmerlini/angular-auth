5446b3a8330d426c19260a212e4d3dae3bd04f21
Francisco Merlini <merlinifrancisco@gmail.com>
2023-12-25 13:59:02 -0300

    feat(api-auth): add register and login endpoints

--- libs/api/auth/src/lib/auth.controller.ts
+++ libs/api/auth/src/lib/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { CreateUserDTO, User } from '@angular-auth/libs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() body: CreateUserDTO): Promise<User> {
+    return this.authService.register(body);
+  }
+
+  @Get('login')
+  login(@Body() body: { email: string; password: string }): Promise<User> {
+    const { email, password } = body;
+    return this.authService.login(email, password);
+  }
 }
--- libs/api/auth/src/lib/auth.service.ts
+++ libs/api/auth/src/lib/auth.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
 
+import { CreateUserDTO, User } from '@angular-auth/libs/common';
+
+const mockedUser: User = {
+  id: 1,
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'johndoe@mail.com',
+  city: 'New York',
+  country: {
+    id: 1,
+    name: 'United States',
+    code: 'US',
+  },
+  role: 'admin',
+  token: '1234567890',
+};
+
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  register(user: CreateUserDTO): Promise<User> {
+    console.log('AuthService.register', user);
+    return Promise.resolve(mockedUser);
+  }
+
+  login(email: string, password: string): Promise<User> {
+    console.log('AuthService.login', email, password);
+    return Promise.resolve(mockedUser);
+  }
+}
