4bf8cc9527a531e575844c0cad45fe9c453a428a
Francisco Merlini <merlinifrancisco@gmail.com>
2024-01-14 16:24:18 -0300

    refactor(web-shared): remove mocked data

--- libs/web/shared/data-access/api/src/lib/country.service.ts
+++ libs/web/shared/data-access/api/src/lib/country.service.ts
@@ -1,53 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { Country } from '@angular-auth/libs/common';
 import { APP_CONFIG } from '@angular-auth/libs/web/shared/utils';
-
-const mockedCountries: Country[] = [
-  {
-    id: 1,
-    name: 'Argentina',
-    code: 'AR',
-  },
-  {
-    id: 2,
-    name: 'Bolivia',
-    code: 'BO',
-  },
-  {
-    id: 3,
-    name: 'Brasil',
-    code: 'BR',
-  },
-  {
-    id: 4,
-    name: 'Chile',
-    code: 'CL',
-  },
-  {
-    id: 5,
-    name: 'Colombia',
-    code: 'CO',
-  },
-  {
-    id: 6,
-    name: 'Perú',
-    code: 'PE',
-  },
-  {
-    id: 7,
-    name: 'Uruguay',
-    code: 'UY',
-  },
-  {
-    id: 8,
-    name: 'Venezuela',
-    code: 'VE',
-  },
-];
 
 @Injectable()
 export class CountryService {
@@ -56,7 +13,6 @@ export class CountryService {
   private readonly baseURL = `${this.appConfig.baseURL}/countries`;
 
   getCountries(): Observable<Country[]> {
-    // return this.http.get<Country[]>(this.baseURL);
-    return of(mockedCountries);
+    return this.http.get<Country[]>(this.baseURL);
   }
 }
