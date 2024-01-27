66b31d76cb666b8a6d4e4a615f137e47a71f0c1c
Francisco Merlini <merlinifrancisco@gmail.com>
2024-01-27 11:14:23 -0300

    feat(nest-auth): add user module

--- apps/nest-auth/src/app/app.module.ts
+++ apps/nest-auth/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from '@angular-auth/libs/api/auth';
 import { CoreModule } from '@angular-auth/libs/api/core';
+import { UserModule } from '@angular-auth/libs/api/user';
 
 @Module({
-  imports: [CoreModule, AuthModule],
+  imports: [CoreModule, AuthModule, UserModule],
 })
 export class AppModule {}
