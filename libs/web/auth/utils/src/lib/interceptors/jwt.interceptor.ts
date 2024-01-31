ab0c1a475202211eb1ae5721f6f96411322ef40e
Francisco Merlini <merlinifrancisco@gmail.com>
2024-01-31 14:32:12 -0300

    fix(web-auth): add extra whitelist urls

--- libs/web/auth/utils/src/lib/interceptors/jwt.interceptor.ts
+++ libs/web/auth/utils/src/lib/interceptors/jwt.interceptor.ts
@@ -9,7 +9,7 @@ import { Store } from '@ngrx/store';
 
 import { EMPTY } from 'rxjs';
 
-import { AuthUrlsEnum } from '@angular-auth/libs/common';
+import { AuthUrlsEnum, ControllersEnum } from '@angular-auth/libs/common';
 import { AuthStore } from '@angular-auth/libs/web/auth/data-access/store';
 import { RouterActions } from '@angular-auth/libs/web/shared/data-access/store';
 
@@ -18,15 +18,13 @@ export const jwtInterceptor: HttpInterceptorFn = (
   next: HttpHandlerFn
 ) => {
   const authStore = inject(AuthStore);
-  const store = inject(Store);
 
-  if (req.url.endsWith(AuthUrlsEnum.LOGIN)) {
+  if (
+    req.url.endsWith(AuthUrlsEnum.LOGIN) ||
+    (req.url.endsWith(ControllersEnum.COUNTRY) && req.method === 'GET') ||
+    (req.url.endsWith(ControllersEnum.USER) && req.method === 'POST')
+  ) {
     return next(req);
-  }
-
-  if (authStore.isRefreshing) {
-    console.log('isRefreshing');
-    return EMPTY;
   }
 
   if (req.url.endsWith(AuthUrlsEnum.REFRESH_TOKEN)) {
@@ -34,8 +32,12 @@ export const jwtInterceptor: HttpInterceptorFn = (
     return next(req);
   }
 
+  if (authStore.isRefreshing) {
+    return EMPTY;
+  }
+
   if (!authStore.accessToken) {
-    store.dispatch(RouterActions.go(['/']));
+    inject(Store).dispatch(RouterActions.go(['/login']));
     return EMPTY;
   }
 
