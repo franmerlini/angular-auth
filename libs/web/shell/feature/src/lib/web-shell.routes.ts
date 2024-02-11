4344ae3638671dae841694eba2689a2d3697d417
Francisco Merlini <merlinifrancisco@gmail.com>
2024-02-11 16:16:54 -0300

    feat(web-shell): add redirect route

--- libs/web/shell/feature/src/lib/web-shell.routes.ts
+++ libs/web/shell/feature/src/lib/web-shell.routes.ts
@@ -4,12 +4,14 @@ import { provideEffects } from '@ngrx/effects';
 import { provideState } from '@ngrx/store';
 
 import { LoginComponent } from '@angular-auth/libs/web/auth/feature/login';
+import { RedirectComponent } from '@angular-auth/libs/web/auth/feature/redirect';
 import { RegisterComponent } from '@angular-auth/libs/web/auth/feature/register';
 import {
   countriesGuard,
   existsUserGuard,
   isAuthenticatedGuard,
   isNotAuthenticatedGuard,
+  redirectParamsGuard,
 } from '@angular-auth/libs/web/auth/utils';
 import { CountryService } from '@angular-auth/libs/web/shared/data-access/api';
 import {
@@ -45,6 +47,11 @@ export const webShellRoutes: Route[] = [
     path: 'login',
     component: LoginComponent,
     canActivate: [isNotAuthenticatedGuard()],
+  },
+  {
+    path: 'redirect',
+    component: RedirectComponent,
+    canActivate: [redirectParamsGuard()],
   },
   {
     path: '**',
