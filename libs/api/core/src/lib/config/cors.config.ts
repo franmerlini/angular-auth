ff6aa918c744f561eb8433c8ca83cf504f8faa60
Francisco Merlini <merlinifrancisco@gmail.com>
2024-02-11 13:34:03 -0300

    refactor(api-core): add specific cors origin

--- libs/api/core/src/lib/config/cors.config.ts
+++ libs/api/core/src/lib/config/cors.config.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const cors: CorsOptions = {
-  origin: '*',
+  origin: 'http://localhost:4200',
   methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
   credentials: true,
   allowedHeaders: 'Content-Type, Authorization',
