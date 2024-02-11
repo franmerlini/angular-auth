b306e1eb24c8128d1afe419ed728ec063d8493d5
Francisco Merlini <merlinifrancisco@gmail.com>
2024-02-11 13:39:21 -0300

    fix(common): add missing keys

--- libs/common/src/lib/enums/auth-endpoints.enum.ts
+++ libs/common/src/lib/enums/auth-endpoints.enum.ts
@@ -1,4 +1,5 @@
 export const AuthEndpointsEnum = {
+  GOOGLE_LOGIN: 'google',
   LOGIN: 'login',
   REFRESH_TOKEN: 'refresh-token',
 } as const;
--- libs/common/src/lib/enums/auth-urls.enum.ts
+++ libs/common/src/lib/enums/auth-urls.enum.ts
@@ -2,6 +2,7 @@ import { AuthEndpointsEnum } from './auth-endpoints.enum';
 import { ControllersEnum } from './controllers.enum';
 
 export const AuthUrlsEnum = {
+  GOOGLE_LOGIN: `/${ControllersEnum.AUTH}/${AuthEndpointsEnum.GOOGLE_LOGIN}`,
   LOGIN: `/${ControllersEnum.AUTH}/${AuthEndpointsEnum.LOGIN}`,
   REFRESH_TOKEN: `/${ControllersEnum.AUTH}/${AuthEndpointsEnum.REFRESH_TOKEN}`,
 } as const;
