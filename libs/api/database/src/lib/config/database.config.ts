64c399268d970007d4dd42f809d3b64baf2d06cc
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-07 21:56:47 -0300

    refactor(api-database): add number casting

--- libs/api/database/src/lib/config/database.config.ts
+++ libs/api/database/src/lib/config/database.config.ts
@@ -2,7 +2,7 @@ export const databaseConfig = () => ({
   db: {
     type: 'mysql' as const,
     host: process.env['NX_DB_HOST'],
-    port: +process.env['NX_DB_PORT'],
+    port: Number(process.env['NX_DB_PORT']),
     username: process.env['NX_DB_USERNAME'],
     password: process.env['NX_DB_PASSWORD'],
     database: process.env['NX_DB_NAME'],
