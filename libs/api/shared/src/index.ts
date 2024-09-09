02aac0ea9cbe1781119524cf2a50ff78500d6f0f
Francisco Merlini <merlinifrancisco@gmail.com>
2024-09-08 23:30:23 -0300

    refactor(api-shared): update barrel file

--- libs/api/shared/src/index.ts
+++ libs/api/shared/src/index.ts
@@ -1,3 +1,6 @@
 export * from './lib/decorators';
+export * from './lib/dtos';
+export * from './lib/enums';
+export * from './lib/exceptions';
 export * from './lib/filters';
 export * from './lib/pipes';
