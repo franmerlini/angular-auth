886608f63b65998e72799262de356ff508717494
Francisco Merlini <merlinifrancisco@gmail.com>
2024-09-09 22:59:56 -0300

    refactor(auth-service): add jwt properties

--- apps/auth-service/src/app/model/environment-variables.model.ts
+++ apps/auth-service/src/app/model/environment-variables.model.ts
@@ -1,8 +1,14 @@
 export type EnvironmentVariables = {
+  // server
   NX_SERVER_PORT: string;
   NX_SERVER_HOST: string;
-  NX_HASH_SALT: string;
+
+  // user-service
+  NX_USER_SERVICE_HOST: string;
+  NX_USER_SERVICE_PORT: string;
+
+  // jwt
   NX_JWT_SECRET: string;
-  NX_GOOGLE_CLIENT_ID: string;
-  NX_GOOGLE_CLIENT_SECRET: string;
+  NX_JWT_TOKEN_EXPIRES_IN: string;
+  NX_JWT_REFRESH_EXPIRES_IN: string;
 };
