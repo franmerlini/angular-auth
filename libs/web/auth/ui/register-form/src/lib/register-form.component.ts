7af35a694025ad37f6ee152ef8618e97744b9611
Francisco Merlini <merlinifrancisco@gmail.com>
2024-09-09 22:37:13 -0300

    refactor(web-auth): add FormKeys and passwordMatchingValidator to form

--- libs/web/auth/ui/register-form/src/lib/register-form.component.ts
+++ libs/web/auth/ui/register-form/src/lib/register-form.component.ts
@@ -2,19 +2,31 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output
 import { FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
-import { Country, CreateUserDTO } from '@angular-auth/libs/shared';
+import { CreateUserDto } from '@angular-auth/libs/api/shared';
+import { Country } from '@angular-auth/libs/shared';
 import { CheckboxComponent, InputComponent, SelectComponent } from '@angular-auth/libs/web/shared/ui';
 import { CustomValidators } from '@angular-auth/libs/web/shared/utils';
 
+const FormKeys = {
+  firstName: 'firstName',
+  lastName: 'lastName',
+  email: 'email',
+  password: 'password',
+  confirmPassword: 'confirmPassword',
+  city: 'city',
+  country: 'country',
+  acceptTerms: 'acceptTerms',
+} as const;
+
 type RegisterForm = {
-  firstName: FormControl<string>;
-  lastName: FormControl<string>;
-  email: FormControl<string>;
-  password: FormControl<string>;
-  confirmPassword: FormControl<string>;
-  city: FormControl<string>;
-  country: FormControl<number>;
-  acceptTerms: FormControl<boolean>;
+  [FormKeys.firstName]: FormControl<string>;
+  [FormKeys.lastName]: FormControl<string>;
+  [FormKeys.email]: FormControl<string>;
+  [FormKeys.password]: FormControl<string>;
+  [FormKeys.confirmPassword]: FormControl<string>;
+  [FormKeys.city]: FormControl<string>;
+  [FormKeys.country]: FormControl<number>;
+  [FormKeys.acceptTerms]: FormControl<boolean>;
 };
 
 @Component({
@@ -27,7 +39,7 @@ type RegisterForm = {
 export class RegisterFormComponent implements OnInit {
   @Input({ required: true }) countryList!: Country[];
 
-  @Output() register = new EventEmitter<CreateUserDTO>();
+  @Output() register = new EventEmitter<CreateUserDto>();
   @Output() googleRegister = new EventEmitter<void>();
 
   private readonly fb = inject(NonNullableFormBuilder);
@@ -39,20 +51,21 @@ export class RegisterFormComponent implements OnInit {
   }
 
   private initForm(): void {
-    this.form = this.fb.group<RegisterForm>({
-      firstName: this.fb.control('', [Validators.required]),
-      lastName: this.fb.control('', [Validators.required]),
-      email: this.fb.control('', [Validators.email, Validators.required]),
-      password: this.fb.control('', [Validators.required, Validators.minLength(8)]),
-      confirmPassword: this.fb.control('', [
-        Validators.required,
-        Validators.minLength(8),
-        CustomValidators.passwordMatchingValidator(this.password, this.confirmPassword),
-      ]),
-      city: this.fb.control('', [Validators.required]),
-      country: this.fb.control(0, [CustomValidators.requiredSelectValidator]),
-      acceptTerms: this.fb.control(false, [Validators.requiredTrue]),
-    });
+    this.form = this.fb.group<RegisterForm>(
+      {
+        [FormKeys.firstName]: this.fb.control('', [Validators.required]),
+        [FormKeys.lastName]: this.fb.control('', [Validators.required]),
+        [FormKeys.email]: this.fb.control('', [Validators.email, Validators.required]),
+        [FormKeys.password]: this.fb.control('', [Validators.required, Validators.minLength(8)]),
+        [FormKeys.confirmPassword]: this.fb.control('', [Validators.required, Validators.minLength(8)]),
+        [FormKeys.city]: this.fb.control('', [Validators.required]),
+        [FormKeys.country]: this.fb.control(0, [CustomValidators.requiredSelectValidator]),
+        [FormKeys.acceptTerms]: this.fb.control(false, [Validators.requiredTrue]),
+      },
+      {
+        validators: [CustomValidators.passwordMatchingValidator(FormKeys.password, FormKeys.confirmPassword)],
+      },
+    );
   }
 
   onSubmit(): void {
@@ -74,34 +87,34 @@ export class RegisterFormComponent implements OnInit {
   }
 
   get firstName(): FormControl<string> {
-    return this.form?.get('firstName') as FormControl<string>;
+    return this.form?.get(FormKeys.firstName) as FormControl<string>;
   }
 
   get lastName(): FormControl<string> {
-    return this.form?.get('lastName') as FormControl<string>;
+    return this.form?.get(FormKeys.lastName) as FormControl<string>;
   }
 
   get email(): FormControl<string> {
-    return this.form?.get('email') as FormControl<string>;
+    return this.form?.get(FormKeys.email) as FormControl<string>;
   }
 
   get password(): FormControl<string> {
-    return this.form?.get('password') as FormControl<string>;
+    return this.form?.get(FormKeys.password) as FormControl<string>;
   }
 
   get confirmPassword(): FormControl<string> {
-    return this.form?.get('confirmPassword') as FormControl<string>;
+    return this.form?.get(FormKeys.confirmPassword) as FormControl<string>;
   }
 
   get city(): FormControl<string> {
-    return this.form?.get('city') as FormControl<string>;
+    return this.form?.get(FormKeys.city) as FormControl<string>;
   }
 
   get country(): FormControl<number> {
-    return this.form?.get('country') as FormControl<number>;
+    return this.form?.get(FormKeys.country) as FormControl<number>;
   }
 
   get acceptTerms(): FormControl<boolean> {
-    return this.form?.get('acceptTerms') as FormControl<boolean>;
+    return this.form?.get(FormKeys.acceptTerms) as FormControl<boolean>;
   }
 }
